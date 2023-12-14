68d5579a48befe29df243788da3a416630ad8204
narhaFaran <146111613+narhaFaran@users.noreply.github.com>
2023-12-14 17:08:25 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Stuff stuff = new Stuff(10);
+        stuff.printAddAmount(6);
+        System.out.println(stuff.getValue());
     }
 }
